f67a62f614e1508ce4127b32bedd99c241912d2d
kalguns <kalguns@gmail.com>
2020-12-02 21:31:04 +0100

    Ticketing Project - MVC

--- src/main/java/com/cybertek/controller/UserController.java
+++ src/main/java/com/cybertek/controller/UserController.java
@@ -1,6 +1,9 @@
 package com.cybertek.controller;
 
+import com.cybertek.dto.RoleDTO;
+import com.cybertek.dto.UserDTO;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -9,7 +12,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class UserController {
 
     @GetMapping("/create")
-    public String createUser(){
+    public String createUser(Model model){
+
+        model.addAttribute("user",new UserDTO());
+
+
         return "/user/create";
     }
 }
